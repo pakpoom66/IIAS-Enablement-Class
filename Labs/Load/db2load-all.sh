3cac1e907e158ec755d982474516558e14c65191
DANIEL A. HANCOCK <Daniel.Hancock@us.ibm.com>
2018-02-03 09:19:07 +0100

    Update db2load-all.sh

--- Labs/Load/db2load-all.sh
+++ Labs/Load/db2load-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 user=$USER
-password=Sailfish@2017
+password=Sailfish@2018
 dbname=BLUDB
 tbschema=$user
-hostnm=9.30.106.50
-datdir=/scratch/home/poc/data
+hostnm=9.30.106.XXX
+datdir=/scratch/home/poc/IBM/data
 outdir=/scratch/home/${user}/load/log
 
 mkdir -p $outdir
